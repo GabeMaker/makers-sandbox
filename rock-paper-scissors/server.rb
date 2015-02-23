4d7e605e81c25e743c8c4be30f34de3adb083aeb
gabe <GabeMaker@users.noreply.github.com>
2015-02-23 14:34:01 +0000

    more work on rps

--- rock-paper-scissors/server.rb
+++ rock-paper-scissors/server.rb
@@ -6,39 +6,36 @@ class RPS < Sinatra::Base
 
   get '/' do
     GAME = Game.new
-    # GAME.clear_game
     computer  = Player.new(name: "computer player")
-    computer.pick [:rock, :paper, :scissors].sample
+
+    # computer.pick [:rock, :paper, :scissors].sample
     GAME.add computer
-    p 0, GAME.players
+
     erb :name
   end
 
   post '/play' do
-    p 1, GAME.players
+    GAME.player_one.pick [:rock, :paper, :scissors].sample
+    GAME.players.pop if GAME.players.count == 2
     @name     = params[:name]
     user      = Player.new(name: @name)
     GAME.add user
-    p 2, GAME.players
-    
+
     erb :play
   end
 
   post '/result' do
-    p 3, GAME.players
+
     @user_shape     = GAME.player_two.pick params.values.first.to_sym
     @computer_shape = GAME.player_one.shape
-    p 4, GAME.players
-    
-
     @name = GAME.player_two.name
+
     if GAME.winner == GAME.player_one
       @winner = :computer
-    elsif GAME.winner == GAME.player_two  
+    elsif GAME.winner == GAME.player_two
       @winner = :user
     end
 
-    # GAME.clear_game
     erb :result
   end
 
