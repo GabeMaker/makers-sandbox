051a17f604187ea57e1ab1e79a92ea076aa2eaf6
gabe <GabeMaker@users.noreply.github.com>
2015-03-01 12:36:51 +0000

    spec demo working

--- chitter-walkthrough/spec/spec_helper.rb
+++ chitter-walkthrough/spec/spec_helper.rb
@@ -18,7 +18,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 ENV['RACK_ENV'] = 'test'
-require 'server'
+require './app'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
