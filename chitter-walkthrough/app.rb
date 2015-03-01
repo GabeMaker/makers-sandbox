895ef529e743d66970430f6a1a08d6af5c6927d7
gabe <GabeMaker@users.noreply.github.com>
2015-03-01 12:14:59 +0000

    rename db chitter_dev, add rspec

--- chitter-walkthrough/app.rb
+++ chitter-walkthrough/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
 
-env = ENV['RACK_ENV'] || 'development'
+env = ENV['RACK_ENV'] || 'dev'
 
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
