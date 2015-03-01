3754f8edda8f55441d52fa0ecef6be721ad69529
gabe <GabeMaker@users.noreply.github.com>
2015-03-01 16:53:50 +0000

    add user signup spec

---
+++ chitter-walkthrough/spec/features/user_management_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature "User signs up" do
+
+scenario "when a new user visits the site" do
+    expect{ sign_up }.to change(User, :count).by(1)
+    expect(page).to have_content("Hi GabeMaker!")
+    expect(User.first.username).to eq("GabeMaker")
+  end
+
+  def sign_up(username = GabeMaker,
+              password = testcoderepeat)
+    visit '/sign_up'
+    expect(page.status_code).to eq(200)
+    fill_in :username, :with => username
+    fill_in :password, :with => password
+    click_button "Sign up!"
+  end
+
+end
