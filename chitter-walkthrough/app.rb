7ff45e41cd703c7948c246617761e44a91ce0896
gabe <GabeMaker@users.noreply.github.com>
2015-03-01 12:11:12 +0000

    initialise data mapper

--- chitter-walkthrough/app.rb
+++ chitter-walkthrough/app.rb
@@ -1,4 +1,14 @@
 require 'sinatra'
+require 'data_mapper'
+
+env = ENV['RACK_ENV'] || 'development'
+
+DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+
+require './lib/cheet'
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do
   'Chitter'
