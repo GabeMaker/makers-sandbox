7ef6a7fdbeafec93f5f17c566203dabf4c5f9f41
gabe <GabeMaker@users.noreply.github.com>
2015-04-11 21:03:25 +0100

    year test

--- date-formatter/lib/formatter.rb
+++ date-formatter/lib/formatter.rb
@@ -2,9 +2,9 @@ class String
 
   def formatter
     if self[-2] == "0"
-      return "April " + "#{self[-1]}" + " 2015"
+      return "April " + "#{self[-1]}" + " #{self[0,4]}"
     else
-      return "April " + "#{self[-2..-1]}" + " 2015"
+      return "April " + "#{self[-2..-1]}" + " #{self[0,4]}"
     end
   end
 
--- date-formatter/spec/formatter_spec.rb
+++ date-formatter/spec/formatter_spec.rb
@@ -6,6 +6,11 @@ describe 'formatter' do
     date2 = "2015-04-01"
     expect(date.formatter).to eq ("April 11 2015")
     expect(date2.formatter).to eq ("April 1 2015")
-
   end
+
+  it 'recognises the year' do
+    date = "2016-04-11"
+    expect(date.formatter).to eq ("April 11 2016")
+  end
+
 end
