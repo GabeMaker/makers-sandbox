dcccdb13c0d7f1f1b0a691a8d56b085e36f02a6c
gabe <GabeMaker@users.noreply.github.com>
2015-04-12 21:10:26 +0100

    refactor

--- date-formatter/lib/formatter.rb
+++ date-formatter/lib/formatter.rb
@@ -8,11 +8,12 @@ class String
     end
     month = months[month_no.to_i-1]
 
+    day = self[-2..-1]
     if self[-2] == "0"
-      return month + " #{self[-1]}" + " #{self[0,4]}"
-    else
-      return month + " #{self[-2..-1]}" + " #{self[0,4]}"
+      day = self[-1]
     end
+
+    return month + " " + day + " #{self[0,4]}"
   end
 
 end
