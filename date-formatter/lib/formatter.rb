2c8459423cead207b7df126eab5181b36594c5e0
gabe <GabeMaker@users.noreply.github.com>
2015-04-12 21:14:28 +0100

    refactor

--- date-formatter/lib/formatter.rb
+++ date-formatter/lib/formatter.rb
@@ -2,18 +2,13 @@ class String
 
   def formatter
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
-    month_no = self[5..6]
-    if self[5] == 0
-      month_no = self[6]
-    end
+    self[5] == 0 ? month_no = self[6] : month_no = self[5..6]
     month = months[month_no.to_i-1]
 
-    day = self[-2..-1]
-    if self[-2] == "0"
-      day = self[-1]
-    end
+    self[-2] == "0" ? day = self[-1] : day = self[-2..-1]
 
     return month + " " + day + " #{self[0,4]}"
   end
 
 end
+
