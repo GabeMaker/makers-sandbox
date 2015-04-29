ff1d5726f8e64a3fd1470908a948c4c5c27d4e88
gabe <GabeMaker@users.noreply.github.com>
2015-04-29 23:27:10 +0100

    refactor

--- largest-element/lib/lgst.rb
+++ largest-element/lib/lgst.rb
@@ -1,21 +1,12 @@
 class Array
 
   def lgst
-    if self.count == 0
-      nil
-    elsif self.count == 1
-      self.first
-    else
-      largest = 0
-      self.each do |x|
-        if x > largest
-          largest = x
-        else
-          next
-        end
-      end
-      return largest
+    return nil if self.count == 0
+    largest = 0
+    self.each do |x|
+      largest = x if x > largest
     end
+    return largest
   end
 
 end
