eddb3d8f75d9b5168a04d0654e8511379ea80ee0
gabe <GabeMaker@users.noreply.github.com>
2015-04-29 23:51:56 +0100

    refactor

--- largest-element/lib/lgst.rb
+++ largest-element/lib/lgst.rb
@@ -1,12 +1,13 @@
 class Array
 
+  # writing method without using #max
+
   def lgst
-    return nil if self.count == 0
-    largest = 0
+    self.count == 0 ? lgst = nil : lgst = 0
     self.each do |x|
-      largest = x if x > largest
+      lgst = x if x > lgst
     end
-    return largest
+    return lgst
   end
 
 end
