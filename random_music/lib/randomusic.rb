2645e22b437f7dfaca6434228c4e24b04d4e46af
gabe <GabeMaker@users.noreply.github.com>
2015-05-08 17:30:51 +0100

    add code to prevent duplicate pages

--- random_music/lib/randomusic.rb
+++ random_music/lib/randomusic.rb
@@ -2,7 +2,13 @@ class Randomusic
 
   def tracks year
     result = []
-    5.times { |x| result << rand(435) + 21 }
+    5.times do |x|
+      new_page = 0
+      until new_page != 0 && result.include?(new_page) == false do
+        new_page = rand(435) + 21
+      end
+      result << new_page
+    end
     result
   end
 
