1eaf124fa1229ac86145a028de41ca605f393752
gabe <GabeMaker@users.noreply.github.com>
2015-05-12 23:19:08 +0100

    returns fizzbuzz for multiples of 3 AND 5

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,7 +1,6 @@
 def fizzbuzz number
-  if number % 3 == 0 || number == 5
-    number % 3 == 0 ? 'fizz' : 'buzz'
-  else
-    number
-  end
+  return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number % 5 == 0
+  number
 end
--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -11,16 +11,30 @@ describe 'fizzbuzz' do
   end
 
   it 'returns the number when passed a number less than 20 and not divisible by 3 or 5' do
-    numbers = [1,2,4,7,8,10,11,13,14,16,17,19]
+    numbers = [1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19]
     numbers.each do |x|
       expect(fizzbuzz(x)).to eq x
     end
   end
 
-  it 'returns "fizz when passed multiples of 3 (not divisible by 5)' do
-    multiples = [3,6,9,12,18]
+  it 'returns "fizz" when passed multiples of 3 (not divisible by 5)' do
+    multiples = [3, 6, 9, 12, 18, 21, 24, 27]
     multiples.each do |x|
       expect(fizzbuzz(x)). to eq 'fizz'
+    end
+  end
+
+  it 'returns "buzz" when passed multiples of 5 (not divisible by 3)' do
+    multiples = [5, 10, 20, 25]
+    multiples.each do |x|
+      expect(fizzbuzz(x)). to eq 'buzz'
+    end
+  end
+
+  it 'returns "fizzbuzz" when a multiple of both 3 and 5' do
+    multiples = [15, 30]
+    multiples.each do |x|
+      expect(fizzbuzz(x)). to eq 'fizzbuzz'
     end
   end
 
