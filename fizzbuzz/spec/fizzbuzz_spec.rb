8a3b7677e30215c6540bbeae49e3114edb485b72
gabe <GabeMaker@users.noreply.github.com>
2015-05-12 23:22:44 +0100

    remove redundant tests

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -2,21 +2,6 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)). to eq 'fizz'
-  end
-
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)). to eq 'buzz'
-  end
-
-  it 'returns the number when passed a number less than 20 and not divisible by 3 or 5' do
-    numbers = [1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19]
-    numbers.each do |x|
-      expect(fizzbuzz(x)).to eq x
-    end
-  end
-
   it 'returns "fizz" when passed multiples of 3 (not divisible by 5)' do
     multiples = [3, 6, 9, 12, 18, 21, 24, 27]
     multiples.each do |x|
@@ -35,6 +20,13 @@ describe 'fizzbuzz' do
     multiples = [15, 30]
     multiples.each do |x|
       expect(fizzbuzz(x)). to eq 'fizzbuzz'
+    end
+  end
+
+  it 'returns the number itself when passed a number not divisible by 3 or 5' do
+    numbers = [1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19]
+    numbers.each do |x|
+      expect(fizzbuzz(x)).to eq x
     end
   end
 
