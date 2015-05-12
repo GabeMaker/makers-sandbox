c27a0d0050d3a0df926c84695a7f2e72821e0a2a
gabe <GabeMaker@users.noreply.github.com>
2015-05-12 23:04:53 +0100

    passes third test

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz number
-  number == 3 ? 'fizz' : 'buzz'
+  if number == 3 || number == 5
+    number == 3 ? 'fizz' : 'buzz'
+  else
+    number
+  end
 end
--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -10,4 +10,10 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)). to eq 'buzz'
   end
 
+  it 'returns the number when passed a number less than 20 and not divisible by 3 or 5' do
+    numbers = [1,2,4,7,8,10,11,13,14,16,17,19]
+    numbers.each do |x|
+      expect(fizzbuzz(x)).to eq x
+    end
+  end
 end
