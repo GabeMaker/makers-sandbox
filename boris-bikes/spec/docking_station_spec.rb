54988499c9e1d23773054bfd0afa10836b26a1df
gabe <GabeMaker@users.noreply.github.com>
2015-05-13 17:36:05 +0100

    DS raises error if no bikes available

--- boris-bikes/spec/docking_station_spec.rb
+++ boris-bikes/spec/docking_station_spec.rb
@@ -1,10 +1,23 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe 'release_bike' do
+
+    it { is_expected.to respond_to :release_bike }
+
+    it 'releases working bikes' do
+      subject.dock Bike.new
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
 end
