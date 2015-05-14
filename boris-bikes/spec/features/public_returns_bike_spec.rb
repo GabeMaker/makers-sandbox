fad6e941bf8c858cc9fe32321bacb6a38d99fee7
gabe <GabeMaker@users.noreply.github.com>
2015-05-14 23:14:13 +0100

    bike can be reported broken

--- boris-bikes/spec/features/public_returns_bike_spec.rb
+++ boris-bikes/spec/features/public_returns_bike_spec.rb
@@ -4,4 +4,13 @@ feature 'member of public returns bike' do
     docking_station.capacity.times { docking_station.dock Bike.new }
     expect { docking_station.dock Bike.new }.to raise_error 'Docking station is full'
   end
+
+  scenario 'bike can be reported broken when returned' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    bike.report_broken
+    expect(bike).to_not be_working
+    expect { docking_station.dock bike }.not_to raise_error
+  end
+
 end
