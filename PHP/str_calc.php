a8760560991cf4208115e027133088d05513f2a4
gabe <GabeMaker@users.noreply.github.com>
2015-06-23 21:53:17 +0100

    'test': function returns 0 when given no arguments

--- PHP/str_calc.php
+++ PHP/str_calc.php
@@ -1,13 +1,16 @@
 <?php
 
   function add($number1, $number2) {
+    $number1 = is_null ($number1) ? 0 : $number1;
+    $number2 = is_null ($number2) ? 0 : $number2;
+
     return ($number1 + $number2);
   }
 
-  $x = "1";
-  $y = "2";
-  $z = "3";
+  $x = "4";
+  $y = "5";
 
-  print add($x, $y)."\n";
+  # 'test' for taking no arguments: returns 0
+  print add()."\n";
 
 ?>
