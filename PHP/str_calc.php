88b65338aac8bb045e9c5505d92788188c9221cb
gabe <GabeMaker@users.noreply.github.com>
2015-06-24 21:32:21 +0100

    can take unknown number of arguments

--- PHP/str_calc.php
+++ PHP/str_calc.php
@@ -3,6 +3,12 @@
   error_reporting(0);
 
   function add($number1, $number2) {
+
+    $arguments = func_get_args();
+    echo $arguments[2];
+
+
+
     $number1 = is_null ($number1) ? 0 : $number1;
     $number2 = is_null ($number2) ? 0 : $number2;
 
@@ -17,4 +23,8 @@
 
   # 'test' for taking empty string: returns 0
   print add("")."\n";
+
+  # in order to add an unknown number of arguments
+  # 'test' for being able to access a 3rd argument - in: returns 6
+  print add(4,5,6)."\n";
 ?>
